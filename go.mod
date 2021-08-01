96fd2f92d8d83bec99c3797d9b0b1c746f321bc8
dschila <dennis@schilakowski.de>
2021-08-01 16:31:49 +0200

    Add default cors config

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/dschila/geizhals-api-go
 go 1.16
 
 require (
+	github.com/gin-contrib/cors v1.3.1 // indirect
 	github.com/gin-gonic/gin v1.7.2
 	github.com/gocolly/colly/v2 v2.1.0
 	github.com/joho/godotenv v1.3.0
+	github.com/rs/cors v1.8.0
 )
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/dschila/geizhals-api-go/controllers"
 	"github.com/dschila/geizhals-api-go/helpers"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
@@ -17,6 +18,8 @@ func main() {
 	}
 
 	server := gin.Default()
+	server.Use(gin.Logger())
+	server.Use(cors.Default())
 
 	defaultGroup := server.Group("/api")
 	controllers.InitSearchController(defaultGroup)
